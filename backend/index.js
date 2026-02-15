fb45bcc5e2aaa9013545f896f359b83c4135a7b6
RAJESHKONATHALA226 <konathalarajesh1@gmail.com>
2026-02-15 12:05:30 +0530

    bhbbhhb

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,7 @@ const app=express();
 
 app.use(cors({
   origin: "https://www.strlearners.site",
+    credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
