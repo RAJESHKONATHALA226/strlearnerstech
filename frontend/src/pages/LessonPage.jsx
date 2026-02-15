bbc2e81e2debde4fb49c6cc8578bf9a38807d45b
RAJESHKONATHALA226 <konathalarajesh1@gmail.com>
2026-02-15 18:34:11 +0530

    vgvg

--- frontend/src/pages/LessonPage.jsx
+++ frontend/src/pages/LessonPage.jsx
@@ -238,6 +238,11 @@ export default function LessonPage() {
       transition
     "
   >
+    <img
+      src={c.userId?.image}
+      alt="user"
+      className="w-10 h-10 rounded-full object-cover mb-2"
+    />
     <p className="font-semibold text-blue-600">
       {c.userId?.name}
     </p>
