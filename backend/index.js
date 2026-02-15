1f370428a2f21e36b0a044d6e84efbc2a4c6cfe2
RAJESHKONATHALA226 <konathalarajesh1@gmail.com>
2026-02-15 12:57:08 +0530

    hukjh

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ connectDB();
 const app=express();
 
 app.use(cors({
-  origin: "https://www.strlearners.site",
+  origin: "https://strlearners.site",
     credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
